d55a7c8592dd2d170ede6e2782ec53a61e5452c2
Ian Feld <ian@MacBookPro.lan>
2025-02-14 16:42:44 -0600

    test branch commit

--- GrinDormsDemo/home/views.py
+++ GrinDormsDemo/home/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
+    #probably need to make a model that stores the apps we want to integrate so this isn't as hard coded
     link_list = {'browse', 'review'}
     context = {"link_list": link_list}
     return render(request, "home/home.html", context)
