fd9f6b2381ee7312a70eeaf4bc242423004c8cbe
Ian Feld <ian@MacBookPro.lan>
2025-03-27 20:28:32 -0500

    yml editing

--- Demos/GrinDormsDemo/browse/tests.py
+++ Demos/GrinDormsDemo/browse/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from .models import Room, Building
+from django.db.utils import IntegrityError
 
 # Create your tests here.
+class RoomModelTests(TestCase):
+    def test_no_duplicate_floors(self):
+        # Check to ensure all rooms are unique in their building
+        test_building = Building(name="TESTBLD", num_floors=3, has_pit=True)
+        test_building.save()
+
+        test_room_1 = Room(building=test_building, number=1000)
+        test_room_2 = Room(building=test_building, number=1000)
+
+        test_room_1.save()
+
+        # Should raise an IntegrityError
+        try:
+            test_room_2.save()
+        except IntegrityError:
+            caught = True
+        else:
+            caught = False
+        
+        self.assertIs(caught, True)
+
+
