675c4fa57831a56e3a0f5824525ce53d62d79015
feldian15 <f_ian@icloud.com>
2025-04-02 11:09:23 -0500

    created building model and set up url template

--- browse/models.py
+++ browse/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+MAXLEN = 50
+
 # Create your models here.
+class Building(models.model):
+    name = models.CharField(max_length=MAXLEN)
+    region = models.CharField(max_length=MAXLEN)
+    num_floors = models.IntegerField()
+    has_pit = models.BooleanField()
+    num_kitchens = models.IntegerField()
+    central_ac = models.BooleanField()
+    num_washers = models.IntegerField()
+    num_dryers = models.IntegerField()
+    elevator = models.BooleanField()
+    gender_specific = models.BooleanField()
+
+    def __str__ (self):
+        return self.name()
---
+++ browse/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+app_name = "browse"
+url_patterns = [
+    
+]
