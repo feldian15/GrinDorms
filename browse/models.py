48b44dac7b50a4c5e0fd45bb36f398969ff698a1
feldian15 <f_ian@icloud.com>
2025-04-02 17:56:17 -0500

    Added Room model

--- browse/models.py
+++ browse/models.py
@@ -3,7 +3,7 @@ from django.db import models
 MAXLEN = 50
 
 # Create your models here.
-class Building(models.model):
+class Building(models.Model):
     name = models.CharField(max_length=MAXLEN)
     region = models.CharField(max_length=MAXLEN)
     num_floors = models.IntegerField()
@@ -17,3 +17,18 @@ class Building(models.model):
 
     def __str__ (self):
         return self.name()
+
+class Room(models.Model):
+    building = models.ForeignKey(Building, related_name="rooms", on_delete=models.CASCADE)
+    number = models.IntegerField()
+    floor = models.IntegerField(editable=False)
+    num_occupants = models.IntegerField()
+    window_direction = models.IntegerChoices
+    floor_bathrooms = models.IntegerField()
+    srd = models.BooleanField()
+    internal_bathroom = models.BooleanField()
+    kitchen = models.BooleanField()
+    common_room = models.BooleanField()
+
+    def __str__ (self):
+        return "building.name %d" % self.number
