0bc4ecb02e424ab60212ed52019e16f0b573b60c
feldian15 <f_ian@icloud.com>
2025-04-04 11:10:25 -0500

    revised demo html template to add more filter options

--- browse/views.py
+++ browse/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render, HttpResponse
+from . models import Room, Building
+from review.models import Review, Image
 
 # Create your views here.
 # View for browse filtering and sorting
 def browse(request):
+    # filtering criteria are region, building, floor, srd, size, 
+    # direction, washers, dryers, elevator, gender specific, rating...
+
+    # Get the full set of rooms first
+    room_list = Room.objects.all()
+
+    
+    
+    # Filter the room list based on any selected criteria
+
+    # Ensure selected criteria is passed back to the template
+
+    # (re)render the template
     return HttpResponse("This is the browse page")
 
 # View for a specific room's details and reviews
