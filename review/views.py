05b3704f14e8d37683295698ff12fc261c9638dc
feldian15 <f_ian@icloud.com>
2025-04-04 19:59:17 -0500

    finished review view logic

--- review/views.py
+++ review/views.py
@@ -1,10 +1,45 @@
 from django.shortcuts import render, HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+from browse.models import Room, Building, Regions
+from .models import Review, Image
 
 # Create your views here.
 # View for adding a new review
 def review(request):
-    return HttpResponse("This is the page for reviewing")
+    # get input params
+    selected_region = request.GET.get("region")
+    selected_building = request.GET.get("building")
+    selected_floor = request.GET.get("floor")
+    if selected_floor:
+        selected_floor = int(selected_floor)
+
+    building_list = Building.objects.filter(region=selected_region)
+
+    if selected_building:
+        floor_list = Building.objects.get(name=selected_building).get_floor_list
+    else: 
+        floor_list = []
+    
+    if selected_floor or selected_floor == 0:
+        room_list = Room.objects.filter(floor=selected_floor, building__name = selected_building)
+    else:
+        room_list = []
+
+    selected_room = int(request.GET.get('room')) if request.GET.get('room') and request.GET.get('room') != 'none' else 0
+
+    context = {
+        "region_list": Regions.choices,
+        "building_list": building_list,
+        # This doesn't keep track of which buildings have pits etc
+        "floor_list": floor_list,
+        "room_list": room_list,
+        "selected_region": selected_region,
+        "selected_building": selected_building,
+        "selected_floor": selected_floor,
+        "selected_room": selected_room
+    }
+
+    return render(request, "review/review.html", context)
 
 # view for viewing user specific reviews
 def my_reviews(request):
