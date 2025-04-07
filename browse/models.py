40cb154ee911d35c6b49011ded85c204fcff522c
feldian15 <f_ian@icloud.com>
2025-04-06 21:24:44 -0500

    wrote some more tests for rooms and buildings

--- browse/models.py
+++ browse/models.py
@@ -35,14 +35,14 @@ class Floors(models.IntegerChoices):
 class Building(models.Model):
     name = models.CharField(max_length=MAXLEN)
     region = models.CharField(max_length=1, choices=Regions.choices, default=Regions.NORTH)
-    num_floors = models.IntegerField()
-    has_pit = models.BooleanField()
-    num_kitchens = models.IntegerField()
-    central_ac = models.BooleanField()
-    num_washers = models.IntegerField()
-    num_dryers = models.IntegerField()
-    elevator = models.BooleanField()
-    gender_specific = models.BooleanField()
+    num_floors = models.IntegerField(default=1)
+    has_pit = models.BooleanField(default=False)
+    num_kitchens = models.IntegerField(default=1)
+    central_ac = models.BooleanField(default=False)
+    num_washers = models.IntegerField(default=1)
+    num_dryers = models.IntegerField(default=1)
+    elevator = models.BooleanField(default=False)
+    gender_specific = models.BooleanField(default=False)
     sub_free = models.BooleanField(default=False)
 
     def __str__ (self):
@@ -56,15 +56,15 @@ class Building(models.Model):
 
 class Room(models.Model):
     building = models.ForeignKey(Building, related_name="rooms", on_delete=models.CASCADE)
-    number = models.IntegerField()
+    number = models.IntegerField(default = 1111)
     floor = models.IntegerField(editable=False, choices=Floors.choices, default=Floors.PIT)
     num_occupants = models.IntegerField(choices=Sizes.choices, default=Sizes.SINGLE)
     window_direction = models.CharField(choices=Directions.choices, default=Directions.NORTH, max_length=1)
-    floor_bathrooms = models.IntegerField()
-    srd = models.BooleanField()
-    internal_bathroom = models.BooleanField()
-    kitchen = models.BooleanField()
-    common_room = models.BooleanField()
+    floor_bathrooms = models.IntegerField(default=1)
+    srd = models.BooleanField(default=False)
+    internal_bathroom = models.BooleanField(default=False)
+    kitchen = models.BooleanField(default=False)
+    common_room = models.BooleanField(default=False)
     avg_rating = models.FloatField(default=0.0)
 
     class Meta:
@@ -82,9 +82,17 @@ class Room(models.Model):
         # Check that the room number is 4 digits
         if not (1000 <= self.number <= 9999):
             raise ValueError("Room number must be a 4-digit number.")
+        
         # set the floor based on the second digit of the room number
         temp = str(self.number)
         self.floor = int(temp[1:2])
+
+        # check that the floor exists in the building
+        if (self.building.has_pit and self.floor >= self.building.num_floors) or ((not self.building.has_pit) and self.floor > self.building.num_floors):
+            raise ValueError("Building %s has %d floors above ground. Room claims to be on floor %d." % (self.building.name, self.building.num_floors, self.floor))
+        if (not self.building.has_pit) and self.floor == 0:
+            raise ValueError("Building %s has no pit" % self.building.name)
+        
         super().save(*args, **kwargs)
 
     # method to calculate the average rating from all reviews of the room
--- browse/tests.py
+++ browse/tests.py
@@ -1,3 +1,80 @@
 from django.test import TestCase
+from .models import Room, Building, Floors
+from django.db.utils import IntegrityError
 
 # Create your tests here.
+class RoomModelTests(TestCase):
+    def test_no_duplicate_floors(self):
+        # Check to ensure all rooms are unique in their building
+        test_building = Building(name="TESTBLD", num_floors=3, has_pit=True)
+        test_building.save()
+
+        test_room_1 = Room(building=test_building, number=1111)
+        test_room_2 = Room(building=test_building, number=1111)
+
+        test_room_1.save()
+
+        # Should raise an IntegrityError
+        try:
+            test_room_2.save()
+        except IntegrityError:
+            caught = True
+        else:
+            caught = False
+        
+        self.assertIs(caught, True)
+
+    def test_correct_floors(self):
+        test_building = Building(name="TESTBLD", num_floors=3, has_pit=True)
+        test_building.save()
+
+        # make sure the highest floor is the second floor
+        floor_list = test_building.get_floor_list()
+
+        # top floor value should be the number of floors minus 1
+        self.assertIs(floor_list[test_building.num_floors - 1][0], test_building.num_floors - 1)
+
+    def test_room_number_is_four_digits(self):
+        test_building = Building(name="TESTBLD", num_floors=3, has_pit=True)
+        test_building.save()
+
+        # should err because room number is invalid
+        test_room = Room(building=test_building, number = 111)
+
+        try:
+            test_room.save()
+        except ValueError:
+            caught = True
+        else:
+            caught = False
+
+        self.assertIs(caught, True)
+    
+    def test_valid_floor_number(self):
+        test_building = Building(name="TESTBLD", num_floors=3, has_pit=True)
+        test_building.save()
+
+        # make a room on the third floor of a building with only 2 floors above ground
+        test_third_floor_room = Room(building=test_building, number=3300)
+
+        try:
+            test_third_floor_room.save()
+        except ValueError:
+            caught1 = True
+        else:
+            caught1 = False
+        
+        test_building_2 = Building(name="TESTBLD2", num_floors=3, has_pit=False)
+        test_building_2.save()
+
+        # make a room in the pit of the building without a pit
+        test_pit_room = Room(building=test_building_2, number=3001)
+
+        try:
+            test_pit_room.save()
+        except ValueError:
+            caught2 = True
+        else:
+            caught2 = False
+
+        self.assertIs((caught1 and caught2), True)
