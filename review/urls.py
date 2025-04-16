cdecb55f18237fe74d984fb27cb1887d4eedeb3d
feldian15 <f_ian@icloud.com>
2025-04-16 11:18:42 -0500

    handle errors associated with posting a second review of a room

--- review/urls.py
+++ review/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.review, name="review"),
     path('my_reviews/', views.my_reviews, name="my_reviews"),
     path('add_success/', views.add_success, name="add_success"),
+    path('add_fail/<error_type>', views.add_fail, name="add_fail"),
     path('delete_success/', views.delete_success, name="delete_success"),
     path('add/<str:building_name>/<int:room_number>/', views.add, name="add"),
     path('delete/', views.delete, name="delete")
