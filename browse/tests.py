7631dd8436c59ed5fdb0029188f44f1f15cd3296
feldian15 <f_ian@icloud.com>
2025-05-06 12:18:45 -0500

    fixed test issue

--- browse/tests.py
+++ browse/tests.py
@@ -83,7 +83,8 @@ class RoomPageTest(StaticLiveServerTestCase):
     def setUpClass(cls):
         super().setUpClass()
         chrome_options = Options()
-        # chrome_options.add_argument("--headless")  # run without opening a window
+        # Uncomment the next line to run tests headlessly (no GUI)
+        chrome_options.add_argument("--headless=new")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
 
